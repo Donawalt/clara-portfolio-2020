fc2f8e63c01ea4c1cce8ae4744ef5f90e5e9b089
Donawalt <donael.walter@hotmail.com>
2020-04-19 23:07:32 +0200

    Seting up prismic

---
+++ nuxt.config.js
@@ -0,0 +1,79 @@
+export default {
+  mode: 'spa',
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '' }]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+  /*
+   ** Global CSS
+   */
+  css: [
+    '@/assets/styles/reset.css'
+  ],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/style-resources'
+  ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    '@nuxtjs/pwa',
+    // Doc: https://github.com/nuxt-community/dotenv-module
+    '@nuxtjs/dotenv',
+    // modules for full static before `nuxt export` (coming in v2.12)
+
+    '@/modules/static',
+
+    '@/modules/crawler',
+
+    // This is where you import the new plugin
+
+    '@nuxtjs/prismic'
+  ],
+  // This is where you configure your settings for the new plugin
+  prismic: {
+    endpoint: 'https://claraportfolio.cdn.prismic.io/api/v2',
+    linkResolver: '@/plugins/link-resolver',
+    htmlSerializer: '@/plugins/html-serializer'
+  },
+  /*
+   ** Styles Ressources
+   */
+  styleResources: {
+    scss: './assets/styles/variables.scss'
+  },
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend (config, ctx) {}
+  }
+}
